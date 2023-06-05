bd29c1f6f49da374bf6b5e3a975ac3ea74823765
P Nikita <92731180+nikita8763@users.noreply.github.com>
2023-06-05 20:26:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const questions = [
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answers: [
+            { text: "var", correct: false},
+            { text: "let", correct: false},
+            { text: "Both A and B", correct: true},
+            { text: "None of the Above", correct: false},
+        ]
+    },
+    {
+        question: "Which type of JavaScript language is ",
+        answers: [
+            { text: "Object-Oriented", correct: false},
+            { text: "Object-Based", correct: true},
+            { text: "Assembly-language", correct: false},
+            { text: "High-level", correct: false},
+        ]
+    },
+    {
+        question: "The HTML attribute used to define the inline styles is",
+        answers: [
+            { text: "style", correct: true},
+            { text: "styles", correct: false},
+            { text: "class", correct: false},
+            { text: "None of the Above", correct: false},
+        ]
+    },
+    {
+        question: "The property in CSS used to change the background color of an element is",
+        answers: [
+            { text: "bgcolor", correct: false},
+            { text: "color", correct: false},
+            { text: "background-color", correct: true},
+            { text: "All of the Above", correct: false},
+        ]
+    },
+    {
+        question: "<input> is",
+        answers: [
+            { text: "a format tag", correct: false},
+            { text: "an empty tag", correct: true},
+            { text: "All of the above", correct: false},
+            { text: "None of the Above", correct: false},
+        ]
+    },
+
+];
